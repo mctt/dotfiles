612a261614e2fc4f80bd46c6735c573a18848299
mctt <7041110+mctt@users.noreply.github.com>
2026-03-08 05:30:56 +0000

    update from unas

--- bin/executable_push_chezmoi_to_git.sh
+++ bin/executable_push_chezmoi_to_git.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # https://claude.ai/share/256f15e2-cd71-409e-9270-7bef52dfbffa
 # chezmoi add ~/.bashrc
-chezmoi cd
-git add .
-git commit -m "update from $(hostname)"
-git push
+
+# this keeps making subshells unless you add exit
+#chezmoi cd
+#git add .
+#git commit -m "update from $(hostname)"
+#git push
+#exit
+
+#testing with the chezmoi git command
+#!/bin/bash
+chezmoi git add .
+chezmoi git commit -- -m "update from $(hostname)"
+chezmoi git push
+# and no need for exit because not subshell is created
+
